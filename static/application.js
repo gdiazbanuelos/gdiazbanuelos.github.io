c3372de65855a5c2a2195914096bf58ad81ffe1e
Gustavo Diaz <gdiazbanuelos@gmail.com>
2020-12-15 11:39:54 -0600

    further api testing

--- static/application.js
+++ static/application.js
@@ -35,7 +35,8 @@ async function fetchText(target, data = {}) {
     credentials: 'same-origin', // include, *same-origin, omit
     headers: {
       //'Content-Type': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded'
+      //'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'text/plain'
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
