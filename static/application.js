64aab12526774708c601c7e99e3f274620608b87
Gustavo Diaz <gdiazbanuelos@gmail.com>
2020-12-15 11:33:50 -0600

    further api testing

--- static/application.js
+++ static/application.js
@@ -52,12 +52,12 @@ async function postData(target, data = {}) {
   // Default options are marked with *
   const response = await fetch(api_url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
+    mode: 'no-cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
+      //'Content-Type': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
