43f603454a7e229894c34c5629e08f2ac475d97c
Gustavo Diaz <gdiazbanuelos@gmail.com>
2020-12-15 10:00:30 -0600

    further api testing

--- static/application.js
+++ static/application.js
@@ -36,7 +36,8 @@ async function fetchText(target) {
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer'
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }
