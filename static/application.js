5785d7147024b482a98a34f582aaf3073a610eae
Gustavo Diaz <gdiazbanuelos@gmail.com>
2020-12-15 11:49:07 -0600

    further api testing

--- static/application.js
+++ static/application.js
@@ -29,7 +29,7 @@ async function fetchText(target, data = {}) {
   // Default options are marked with *
 
   const response = await fetch(api_url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'no-cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
