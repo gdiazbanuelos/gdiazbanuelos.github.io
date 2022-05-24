1d76c172ef30418984faf6f4f70c1aa862c009a6
Gus <gdiazbanuelos@gmail.com>
2022-05-23 20:21:55 -0500

    mor

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import { BsGithub } from 'react-icons/bs'
 
 function App() {
   return (
-    <div id="wrapper" className="block align-baseline max-w-[calc(100%-2em)] mx-auto sm:w-[90em]">
+    <div id="wrapper" className="block align-baseline max-w-[calc(100%-2em)] mx-auto 
+    sm:w-[58em] lg:w-[64em] xl:w-[80em]">
 
       <Helmet>
         <title>Gus Diaz ・ Welcome!</title>
@@ -84,9 +85,9 @@ function App() {
           <header>
             <p className="text-xl mb-3 sm:mb-12 sm:text-5xl">My Other Personal Websites</p>
           </header>
-          <ul className="flex justify-center flex-col sm:flex-row mb-8 sm:mx-20">
-            <li className="sm:mt-8 sm:w-1/2">
-              <a href="https://jp.gusdiaz.com" className="text-[#0000EE] sm:text-3xl">jp.gusdiaz.com</a>
+          <ul className="flex justify-center flex-col xl:flex-row mb-8 xl:mx-20 ">
+            <li className="sm:mt-8 sm:w-1/2 ">
+              <a href="https://jp.gusdiaz.com" className="text-[#0000EE] sm:text-3xl ">jp.gusdiaz.com</a>
               <br/><br />
               <span className="flex justify-center">
                 <img src={yoru} className="pb-3 sm:w-[256px]"/>
@@ -171,50 +172,50 @@ function App() {
 
 
       <section id="footer" className="flex flex-col pt-10 px-4 pb-48 text-white sm:flex-row sm:justify-center">
-        <section className="mb-12">
+        <section className="mb-12 sm:w-1/3 xl:w-2/3">
           <p className="text-xl pb-4 sm:text-3xl">Thank you for visiting my website!</p>
-          <p className="mb-8 sm:w-2/3">I appreciate your time and consideration. Feel free to contact me for any questions via any of the platforms to the right.</p>
+          <p className="mb-8 sm:w-2/3 sm:text-xl">I appreciate your time and consideration. Feel free to contact me for any questions via any of the platforms to the right.</p>
           <ul className="text-center sm:text-left">
             <li>
               <button class="bg-transparent hover:bg-[#F7F7F7] font-semibold border border-[#DDDDDD]
-                             hover:border-transparent rounded w-full sm:w-1/3">
-                <a href="#" className="text-2xl">Back to top of page</a>
+                             hover:border-transparent rounded w-full sm:w-2/3 xl:w-1/3 p-4">
+                <a href="#" className="text-lg sm:text-xl xl:text-2xl">Back to top of page</a>
               </button>
             </li>
           </ul>
         </section>
-        <section className="">
-          <p className="text-xl px-4 font-bold">Contact Information</p>
+        <section className="sm:text-3xl w-1/2 xl:w-1/3">
+          <p className="text-xl px-4 font-bold xl:text-3xl">Contact Information</p>
           <div className="text-white">
             <dl className="">
               <div class="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-bold">Full name</dt>
-                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2">Gustavo Diaz Banuelos</dd>
+                <dt class="text-sm font-bold xl:text-xl">Full name</dt>
+                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2 xl:text-xl">Gustavo Diaz Banuelos</dd>
               </div>
               <div class=" px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-bold">Address</dt>
-                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2">Chicago, IL • USA</dd>
+                <dt class="text-sm font-bold xl:text-xl">Address</dt>
+                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2 xl:text-xl">Chicago, IL • USA</dd>
               </div>
               <div class=" px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-bold">Phone</dt>
-                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2">(214) 734-2809</dd>
+                <dt class="text-sm font-bold xl:text-xl">Phone</dt>
+                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2 xl:text-xl">(214) 734-2809</dd>
               </div>
               <div class=" px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-bold">Email</dt>
-                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2">
-                  <a href="mailto:gdiazbanuelos@gmail.com" className="underline">gdiazbanuelos@gmail.com</a>
+                <dt class="text-sm font-bold xl:text-xl">Email</dt>
+                <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2 xl:text-xl">
+                  <a href="mailto:gdiazbanuelos@gmail.com" className="underline xl:text-xl">gdiazbanuelos@gmail.com</a>
                 </dd>
               </div>
               <div class=" px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-bold">Linkedin</dt>
+                <dt class="text-sm font-bold xl:text-xl">Linkedin</dt>
                 <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2">
-                  <a href="https://www.linkedin.com/in/gdiazbanuelos/" className="underline">linkedin.com/in/gdiazbanuelos</a>
+                  <a href="https://www.linkedin.com/in/gdiazbanuelos/" className="underline xl:text-xl">linkedin.com/in/gdiazbanuelos</a>
                 </dd>
               </div>
               <div class=" px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-bold">GitHub</dt>
+                <dt class="text-sm font-bold xl:text-xl">GitHub</dt>
                 <dd class="mt-1 text-sm  sm:mt-0 sm:col-span-2">
-                  <a href="https://github.com/gdiazbanuelos" className="underline">github.com/gdiazbanuelos</a>
+                  <a href="https://github.com/gdiazbanuelos" className="sm:text-xl underline">github.com/gdiazbanuelos</a>
                 </dd>
               </div>
             </dl>
